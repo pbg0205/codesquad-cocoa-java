6c9d8d3c7abb9f2a1125ee2546d24b7cb63cc7b1
pbg0205 <pbg0205@naver.com>
2020-11-25 22:21:20 +0900

    [mod] Application.java 삭제(콘솔 출력)

--- mission07/src/main/java/game2048/Main.java
+++ mission07/src/main/java/game2048/Main.java
@@ -2,8 +2,6 @@ package game2048;
 
 class Main {
     public static void main(String[] args) {
-//        Application application = new Application();
-//        application.start();
         new GameView();
     }
 }
